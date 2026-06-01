b1a2fe5016a69a3ae1331218f03b6f6b75c816d1
annuarmaan002-gif <annuarmaan002@gmail.com>
2026-06-02 02:57:38 +0530

    Simplify client intents and remove message handling
    Removed unused intents and message event listener.

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
-});
-
-client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
-
-  if (message.content === "ping") {
-    message.reply("Pong! 🏓");
-  }
-
-  if (message.content === "hello") {
-    message.reply("Hello bro 👋");
-  }
 });
 
 client.login(process.env.TOKEN);
